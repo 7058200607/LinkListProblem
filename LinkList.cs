c09102a54ebc2e2fb311ce5c144eccb85078a112
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-26 14:47:07 +0530

    Added 56 30 70 element in link list

---
+++ LinkList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListProblem
+{
+    public class LinkList
+    {
+        internal node head;
+        internal void Add(int data)
+        {
+            node Node = new node(data);
+            if (head == null)
+            {
+                this.head = Node;
+            }
+            else
+            {
+                node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = Node;
+            }
+            Console.WriteLine("{0} inserted Linked List", Node.data);
+        }
+        internal void Display()
+        {
+            node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkList is Empty ");
+                return;
+            }
+            while (temp != null) 
+            { 
+                Console.Write( temp.data + " ");
+                temp = temp.next;
+           
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListProblem
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Do LinkList Operation ");
+            LinkList linklist =new LinkList();
+            linklist.Add(56);
+            linklist.Add(30);
+            linklist.Add(70);
+
+            linklist.Display();
+
+
+        }
+
+    }
+}
+    
