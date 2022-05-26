a5f76a1c960fccde06ac407a369378dc6d1f2ec1
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-26 16:38:10 +0530

    Append data and Insert Element Data in Perticular position

--- LinkList.cs
+++ LinkList.cs
@@ -37,10 +37,76 @@ namespace LinkListProblem
             }
             while (temp != null) 
             { 
-                Console.Write( temp.data + " ");
+                Console.WriteLine( temp.data + " ");
                 temp = temp.next;
            
             }
         }
+        public void Appending(int value) 
+        {
+            node Node = new node(value);
+            if (head == null) 
+            {
+                head = Node;
+            }
+            else 
+            {
+                node lastnode = Last();
+                lastnode.next = Node;
+            }
+            Console.WriteLine(Node.data  +  "Inserted into list ");
+
+        }
+        public node Last() 
+        {
+            node temp=head;
+            while(temp.next != null) 
+            {
+                temp=temp.next;
+            }
+            return temp;
+        }
+        internal node InsertAtPerticularPosition(int position, int newelemnet)
+        {
+            node newnode=new node(newelemnet);
+            newnode.data = position;
+            newnode.next = null;
+
+
+            if (position < 1)
+                Console.WriteLine("\n position should be >= 1.");
+            else if (position == 1)
+            {
+                newnode.next = this.head;
+                head = newnode;
+            }
+            else
+            {
+                node temp = new node(newelemnet);
+                temp = head;
+                for (int i = 1; i < position - 1; i++)
+                {
+                    if (temp! == null)
+                    {
+                        temp = temp.next;
+                    }
+                }
+
+                if (temp != null)
+                {
+                    newnode.next = temp.next;
+                    temp.next = newnode;
+                    Console.WriteLine("Position Out of Range");
+                }
+                else
+                {
+                    Console.WriteLine("Inserted element is" + head);
+
+                }
+            }
+            return head;
+    
+        }
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,8 +13,16 @@ namespace LinkListProblem
             Console.WriteLine(" Do LinkList Operation ");
             LinkList linklist =new LinkList();
             linklist.Add(56);
-            linklist.Add(30);
+           // linklist.Add(30);
             linklist.Add(70);
+            linklist.Display();
+            
+            //linklist.Appending(56);
+            //linklist.Appending(30);
+            //linklist.Appending(70);
+            //linklist.Display();
+
+           linklist.InsertAtPerticularPosition(30, 2);
             linklist.Display();
        
         }
